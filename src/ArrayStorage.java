4afcb6bd9183750feab26039f7849ac65ed3e77d
Chupin Ivan <ivan.chupin.95@gmail.com>
2017-10-02 10:56:02 +0300

    ArrayStorage has been realized

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,61 @@
+
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+
+    Resume[] storage;
+    private int i;
+
+    public ArrayStorage() {
+        storage = new Resume[10_000];
+    }
 
     void clear() {
+        storage = new Resume[10_000];
+        i = 0;
     }
 
     void save(Resume r) {
+        storage[i] = r;
+        i++;
     }
 
     Resume get(String uuid) {
+        for (int j = 0; j < i; j++) {
+            if (storage[j].uuid.equals(uuid)) {
+                return storage[j];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int k = 0;
+        Resume[] tmpStorage = new Resume[10_000];
+        for (int j = 0; j < i; j++) {
+            if (!storage[j].uuid.equals(uuid)) {
+                tmpStorage[k] = storage[j];
+                k++;
+            }
+        }
+        storage = tmpStorage;
+        i--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] tmpStorage = new Resume[i];
+        for (int j = 0; j < i; j++) {
+            tmpStorage[j] = storage[j];
+        }
+        return tmpStorage;
     }
 
     int size() {
-        return 0;
+        return i;
     }
 }
